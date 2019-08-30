f2ac15f065602ed26808eac4c0c67446534d06dd
rymrebooks <54714892+rymrebooks@users.noreply.github.com>
2019-08-31 06:48:27 +0700

    Update and rename TJson.h to CppJSons.h

--- CppJSons.h
+++ CppJSons.h
@@ -1,22 +1,22 @@
 /*
-Target design:
-	Utf8 only.
-	Fast, stablity
-	Long time excutation for server side.
-	General/flexible purpose in use.
-	Only string storage for all type values.
-	Float point, number, boolean are used as realm/context.
-Author: Trinhdc
-Email:  rymrebooks@gmail.com
-Donate  @ paypal: rymrebooks@gmail.com 
+TAuthor: Trinhdc
+Email: rymrebooks@gmail.com
+Donate @ paypal: rymrebooks@gmail.com
 License: BSD
 Version: 1.0.0.0
+Target:
+
+    Fast, stablity.
+    Thread safe.
+    Long time excutation for server side.
+    General/flexible purpose in use.
+    Only string storage for all type values.
+    Float point, number, boolean are used as realm/context.
+
 RoadMap:
-	More test to get bugs.
-	Listen to users to improve
-Bugs:
-	+
-	+
+
+    More test to get bugs.
+    Listen to users to improve.
 */
 #ifndef _Use_TJson
 #define _Use_TJson
